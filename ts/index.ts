35c8637420b332a34b3758316add0620316d8327
Alain Lormil <alormil@gmail.com>
2016-08-25 21:56:31 -0400

    Initial project setup

---
+++ ts/index.ts
@@ -0,0 +1,7 @@
+export * from './src/helpers/Oauth';
+export * from './src/methods/Account';
+export * from './src/methods/Events';
+export * from './src/methods/Files';
+export * from './src/methods/Friends';
+export * from './src/methods/Transfers';
+export * from './src/methods/Zips';
