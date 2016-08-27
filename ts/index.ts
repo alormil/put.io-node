6c928e6059bd0e53ec22fe73cbc7d1b489880890
Alain Lormil <alormil@gmail.com>
2016-08-27 14:26:46 -0400

    Only need to export Client class since other classes can be retrieved from it

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,2 @@
 export {Client} from './src/helpers/Client';
-export {Account} from './src/methods/Account';
-export {Events} from './src/methods/Events';
-export {Files} from './src/methods/Files';
-export {Friends} from './src/methods/Friends';
-export {Transfers} from './src/methods/Transfers';
-export {Zips} from './src/methods/Zips';
+
