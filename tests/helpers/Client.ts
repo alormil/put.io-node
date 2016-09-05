b498a7873472dc93652b401da89421d5fe4228ef
Alain Lormil <alormil@gmail.com>
2016-09-05 00:55:23 -0400

    Added type to callback function

--- tests/helpers/Client.ts
+++ tests/helpers/Client.ts
@@ -5,12 +5,12 @@ describe ('Put.io Client', () => {
     describe('Client Object', () => {
         const putioClient: Module.Client = new Module.Client('testToken');
 
-        it('Should be able to get oAuth token', (done) => {
+        it('Should be able to get oAuth token', (done: Function) => {
             expect(putioClient.oAuthToken).to.equal('testToken');
             done();
         });
 
-        it('Should be able to set oAuth token', (done) => {
+        it('Should be able to set oAuth token', (done: Function) => {
             putioClient.oAuthToken = 'newToken';
             expect(putioClient.oAuthToken).to.equal('newToken');
             done();
